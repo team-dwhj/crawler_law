63156ee62b925c113d84b215a84bd92186650c56
kakao <kakao@willkpc.local>
2020-07-12 20:20:49 +0900

    부트스트랩 적용

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, session, render_template, url_for, redirect
 from requests import get
 from os import path
+from csv import reader
 
 
 # configure app
@@ -9,8 +10,18 @@ app.debug = True                                # 파일이 수정될 때마다 
 app.secret_key = 'SuperSecret'                  # flask session 생성 위한 key
 
 
+def get_list(list_name):
+    script_dir = path.dirname(__file__)
+    rel_path = "/form/" + list_name
+
+    ret = []
+    with open(script_dir + rel_path, newline='') as fp:
+        ret = list(reader(fp))
+    print(ret)
+    return ret
+
 @app.route("/", methods=['GET', 'POST'])
-def index():
+def search_case():
     print('index')
     print(session)
 
@@ -19,12 +30,16 @@ def index():
         session.clear()                                 # session에 저장된 (key, value) pair 초기화
         session.permanent = True                        # session의 유효기간을 한달로
 
-        # TODO 캡챠 가져오기
-    
-        return render_template('case_form.html')    # ./templates/case_form.html 렌더링
+        # TODO-HJ 캡챠 가져오기
+
+        # TODO-DW case_form.html에 캡챠 이미지 추가하기 
+        return render_template('case_form.html', sch_bub_nm_list=get_list('sch_bub_nm'), sa_gubun_list=get_list('sa_gubun'))    # ./templates/case_form.html 렌더링
 
     elif request.method == 'POST':
-        session.update(request.form)               # post request로 받아온 사건 정보를 session에 저장
+        session.update(request.form)                # post request로 받아온 사건 정보를 session에 저장
+
+        # TODO-DW 사건검색결과 parsing
+
         return redirect(url_for('gen_doc'))         # gen_doc()함수에 상응하는 주소로 redirect
 
 
@@ -37,7 +52,7 @@ def gen_doc():
         return redirect(get(request.host_url[:-1] + url_for('gen_doc'), {'gen_doc_begin':True}).url)
 
     else:
-        # TODO 문서 생성 (경로는 ./documents에)
+        # TODO-HJ 문서 생성 (경로는 ./static에)
 
         #while not path.isfile('/documents/doc.docx'):   # 파일이 생성될때까지 wait
         #    pass
